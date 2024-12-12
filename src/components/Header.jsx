bf2ed334a548abc006aadfbcf4b908c5beb53f46
Andy Simota <andyiulian.simota@gmail.com>
2024-12-12 15:14:08 +0100

    gen first card

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1 className="text-center">Il mio blog</h1>;
+}
+
+export default Header;
---
+++ src/data/LoL_Data.js
@@ -0,0 +1,30 @@
+const champ = [
+  {
+    id: 1,
+    titolo: "Caitlyn",
+    descrizione:
+      "Renowned as its finest peacekeeper, Caitlyn is also Piltover's best shot at ridding the city of its elusive criminal elements.",
+    immagine:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_30.jpg",
+  },
+  {
+    id: 2,
+    titolo: "Secondo Oggetto",
+    descrizione: "Questa è la descrizione del secondo oggetto.",
+    immagine: "https://example.com/immagine2.jpg",
+  },
+  {
+    id: 3,
+    titolo: "Terzo Oggetto",
+    descrizione: "Questa è la descrizione del terzo oggetto.",
+    immagine: "https://example.com/immagine3.jpg",
+  },
+  {
+    id: 4,
+    titolo: "Quarto Oggetto",
+    descrizione: "Questa è la descrizione del quarto oggetto.",
+    immagine: "https://example.com/immagine4.jpg",
+  },
+];
+
+export default champ;
